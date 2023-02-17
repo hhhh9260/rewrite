e2d8b4139dc552d1e6df2742ef27e0f4a08d8008
hhhh9260 <88217697+hhhh9260@users.noreply.github.com>
2023-02-17 11:27:40 +0800

    Create 123.js

---
+++ 123.js
@@ -0,0 +1,14 @@
+/********************************
+@version     v0.0.3
+@updatetime  2023-01-02
+@tgchannel   https://t.me/ddgksf2021
+@function    酷安去首页广告、信息流广告、评论广告
+@author      ddgksf2013
+[rewrite_remote]
+https://github.com/ddgksf2013/Scripts/raw/master/coolapk.js
+https://raw.githubusercontent.com/hhhh9260/rewrite/main/1aa_spotify.js
+
+
+[mitm]
+hostname = api.coolapk.com,spclient.wg.spotify.com
+*********************************/
