ed072d5e41de896e3a49b49d0e2b1a1ddc86c454
hhhh9260 <88217697+hhhh9260@users.noreply.github.com>
2024-03-05 22:50:36 +0800

    Update 1a_spotify.js

--- 1a_spotify.js
+++ 1a_spotify.js
@@ -1,11 +1,14 @@
-#!name=Spotify(>=iOS15)
-#!desc=2023.10.04 部分解锁premium,音质不能设置为超高(建议登录后再打开脚本,重启app等待脚本生效)
-# 仓库地址 https://github.com/app2smile/rules
-# 功能: 1.去除播放广告 2.歌手/专辑列表正常展示 3.去除随机播放
+#!name=Spotify Premium
 
-[MITM]
-hostname = %APPEND% spclient.wg.spotify.com
+#!desc=部分解锁premium,建议重新登录,音质不能设置为超高
 
-[Script]
-spotify-json = type=http-request,pattern=^https:\/\/spclient\.wg\.spotify\.com\/(artistview\/v1\/artist|album-entity-view\/v2\/album)\/,requires-body=0,script-path=https://raw.githubusercontent.com/app2smile/rules/master/js/spotify-json.js
-spotify-proto = type=http-response,pattern=^https:\/\/spclient\.wg\.spotify\.com\/(bootstrap\/v1\/bootstrap|user-customization-service\/v1\/customize)$,requires-body=1,binary-body-mode=1,max-size=0,script-path=https://raw.githubusercontent.com/app2smile/rules/master/js/spotify-proto.js,script-update-interval=0
+#!icon=https://raw.githubusercontent.com/Koolson/Qure/master/IconSet/Color/Spotify.png
+
+#!homepage=https://whatshub.top
+
+#!author=app2smile
+
+
+^https:\/\/spclient\.wg\.spotify\.com\/(bootstrap\/v1\/bootstrap|user-customization-service\/v1\/customize)$ url script-response-body https://raw.githubusercontent.com/app2smile/rules/master/js/spotify-proto.js
+^https:\/\/spclient\.wg\.spotify\.com\/(artistview\/v1\/artist|album-entity-view\/v2\/album)\/ url script-request-body https://raw.githubusercontent.com/app2smile/rules/master/js/spotify-json.js
+hostname = spclient.wg.spotify.com
